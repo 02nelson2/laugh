1a19d0a1d2f12a7d121d090454e5b371b2954815
02nelson2 <danasman2@yahoo.com>
2025-03-26 21:22:05 +0000

    Revert README - fixing typo

--- backend/src/decorators/multipart/inject-request.decorator.ts
+++ backend/src/decorators/multipart/inject-request.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 // Since pipes dont have direct access to the request object, we need this decorator to inject it
-export const InjectRequest = createParamDecorator(
+export let InjectRequest = createParamDecorator(
   async (data: any, ctx: ExecutionContext) => {
     return {
       data: data,
